8f5418491f4bb94569c724b45a74c65ffebd0ea4
spencerengmann <eng20001@byui.edu>
2024-09-28 18:40:13 -0600

    completed the assignment

---
+++ mission/mission.js
@@ -0,0 +1,23 @@
+
+const themeSelector = document.querySelector('#theme-selector');
+
+function changeTheme() {
+  const body = document.body;
+  const logo = document.querySelector('footer img'); 
+
+  
+  if (themeSelector.value === 'dark') {
+    
+    body.classList.add('dark');
+    
+    logo.src = 'byui-logo_white.png'; 
+  } else {
+    
+    body.classList.remove('dark');
+   
+    logo.src = 'byui-logo_blue.webp'; 
+  }
+}
+
+
+themeSelector.addEventListener('change', changeTheme);
