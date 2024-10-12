8f197313f96ba9937af570455dde41406e57fc39
spencerengmann <eng20001@byui.edu>
2024-10-12 10:07:41 -0600

    updates

---
+++ coolpics/coolpics.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector("button");
+    const navMenu = document.querySelector("nav ul");
+
+    
+    menuButton.addEventListener("click", () => {
+        navMenu.classList.toggle("hide");
+    });
+
+    
+    function handleResize() {
+        if (window.innerWidth > 1000) {
+            navMenu.classList.remove("hide");
+        } else {
+            navMenu.classList.add("hide");
+        }
+    }
+
+    
+    window.addEventListener("resize", handleResize);
+
+   
+    handleResize();
+});
+
